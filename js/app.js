0decbaa023adaa5aff772d33fbe1ad3075493a41
Jonathan Leung <me@jonl.org>
2014-10-11 18:29:18 -0600

    Got menubar scrolling to work

--- js/app.js
+++ js/app.js
@@ -1,9 +1,42 @@
 var App = {};
 
 App.init = function() {
-	console.log("Hello World from App.init in app.js");
 	$(document).foundation();
+	initNavBar();
+
 };
+
+var initNavBar = function() {
+
+	var navToSectionClassMap = {
+		"nav-projects": "notebooks",
+		"nav-about": "about",
+		"nav-contact": "contact"
+	}
+
+	for (var property in navToSectionClassMap) {
+		if (navToSectionClassMap.hasOwnProperty(property)) {
+			var id = property;
+			console.log(id)
+
+			$("#"+id).click((function(id) {
+				return function (e) {
+					e.preventDefault();
+
+					var klass = navToSectionClassMap[id];
+
+					$('html, body').animate({
+						scrollTop: $("."+klass).offset().top
+					}, 1000);
+
+				}
+			})(id));
+		}
+	}
+
+
+}
+
 
 $(document).ready(function() {
 	App.init();
