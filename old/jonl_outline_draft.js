72c6d2997c0055f6a09ef274dadff637eb5af0dc
Jonathan Leung <me@jonl.org>
2015-07-22 19:26:29 -0700

    add old website

---
+++ old/jonl_outline_draft.js
@@ -0,0 +1,38 @@
+:Hi!:
+  My name is Jonathan Leung
+  and I love making things
+
+  I am part :Engineer:, :Composer:, and :Entrepreneur:
+
+  My goal in life is to make people "fundamentally happier"
+
+  You Can Find my Resume Here
+
+    :Engineering:
+      I am studying Computer Engineering 
+      at the University of Pennsylvania. 
+
+      This essentially means that I am taking 
+      Computer Science & Electrical Engineering 
+      classes. 
+
+      I like working with Web as much as I like 
+      working with Embedded technologies. 
+
+      I work in Python, Ruby, Java, C, and know the 
+      Google App Engine, Django, and Rails platforms
+
+      Here are "some of the things that I have built"
+
+        
+
+
+
+    :Composer:
+
+    :Entrepreneur:
+
+    :fundamentally happier:
+
+
+
---
+++ old/test.js
@@ -0,0 +1,11 @@
+title: The beginning
+path: "/Musician/"
+# Hi
+
+Now it is time for me to show everyone what the ability
+
+
+
+title: Jonathan Leung
+path: "/Engineer/ability"
+Welcome to the first
